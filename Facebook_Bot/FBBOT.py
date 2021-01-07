9745fc13b922632e01dc603c9d79058557902f82
evgenij_karaschuk <karaschuk.ev@gmail.com>
2021-01-07 23:35:03 +0200

    Добавил токен страницы ProAuto

---
+++ Facebook_Bot/FBBOT.py
@@ -0,0 +1,7 @@
+def post_on_page():
+    """Создает публикации на странице ПроАвто"""
+    pass
+
+
+token = "EAAtVXT40eBUBAAYisb3i6EhYKTXBFbQJ3jV5NcWDt5VyCwryLrp8mpt1cKWzdEiXuZ\
+C46AxA1MwVAH5wgIxBLZBK7ElPDcnPJikkAzsE70CwNELiyNH9cbKR3s1aYSFlRFqyJkA7w7jLxNx17l0SbZApK0fb0tm4loJ18zOyQZDZD"
