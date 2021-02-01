4744464e45f6828312b327629a2c981a6b04a120
evgenij_karaschuk <karaschuk.ev@gmail.com>
2021-02-02 00:36:30 +0200

    Опубликовал видео

--- Facebook_Bot/FBBOT.py
+++ Facebook_Bot/FBBOT.py
@@ -1,7 +1,19 @@
-def post_on_page():
-    """Создает публикации на странице ПроАвто"""
-    pass
+from facepy import GraphAPI
+import requests
 
 
-token = "EAAtVXT40eBUBAAYisb3i6EhYKTXBFbQJ3jV5NcWDt5VyCwryLrp8mpt1cKWzdEiXuZ\
-C46AxA1MwVAH5wgIxBLZBK7ElPDcnPJikkAzsE70CwNELiyNH9cbKR3s1aYSFlRFqyJkA7w7jLxNx17l0SbZApK0fb0tm4loJ18zOyQZDZD"
+token = '''EAAtVXT40eBUBALfxj4OPAPzbBcZBwZCsmWYgnWZC4itYPilHwY4C5JOPRZC
+h0aF9Y8MAytRT1mzZBgZAr9BUbFhsOow5hGElVFASsPmkNHZAZAUE1UcVcxLLjaJ3j7hOZA41
+Ezqs6EVzop4JXWWQfw1THYhzIwBDGErE2ld1DhdpaTwZDZD'''
+
+fburl = f'https://graph-video.facebook.com/v9.0/115161753503896/videos?access_token={token}'
+desc = titl = 'First video'
+
+
+m = {'description': desc,
+      'title': titl,}
+
+files = {'source': ('2.mp4', open('2.mp4', 'rb'), 'video/mp4')}
+
+r = requests.post(fburl, data=m, files=files)
+print(id)
